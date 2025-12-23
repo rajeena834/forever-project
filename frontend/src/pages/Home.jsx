c6c6e0404b94934ef1bdb87260ae0eb9ff04bdfc
RAJEENA RAJESH KUMAR <rajeenarajesh@RAJEENAs-MacBook-Pro.local>
2025-12-23 21:13:09 +0700

    second commit

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Hero from '../components/Hero'
 import LatestCollection from '../components/LatestCollection'
 import BestSeller from '../components/BestSeller'
-import OurPolicy from '../components/Ourpolicy'
+import OurPolicy from '../components/OurPolicy'
 import NewsLetterbox from '../components/NewsLetterbox'
 
 const Home = () => {
