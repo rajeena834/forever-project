163697813037218a8f5291ac85c56082d1081e83
RAJEENA RAJESH KUMAR <rajeenarajesh@RAJEENAs-MacBook-Pro.local>
2025-12-26 11:27:08 +0700

    cors deleted

--- backend/server.js
+++ backend/server.js
@@ -17,19 +17,19 @@ connectCloudinary()
 // Middlewares
 
 app.use(express.json())
-//app.use(cors())
-app.use(
-  cors({
-    origin: [
-      "https://forever-frontend-beta-lilac.vercel.app",
-      "https://forever-admin-ivory-eight.vercel.app",
-      "http://localhost:5173",
-      "http://localhost:5174",
-    ],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true,
-  })
-);
+app.use(cors())
+// app.use(
+//   cors({
+//     origin: [
+//       "https://forever-frontend-beta-lilac.vercel.app",
+//       "https://forever-admin-ivory-eight.vercel.app",
+//       "http://localhost:5173",
+//       "http://localhost:5174",
+//     ],
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     credentials: true,
+//   })
+// );
 //api endpoints
 app.use('/api/user',userRouter)
 app.use('/api/product',productRouter)
